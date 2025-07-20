acd87db502bfe62f1bfdbe8d75727ee2076d3265
El mehdi Elkabia <149374411+ElmehdiElkabia@users.noreply.github.com>
2025-07-20 16:22:20 +0100

    Update auth.js

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -55,7 +55,7 @@ export const auth = {
 
 // OAuth configuration
 export const oauthConfig = {
-  clientId: 'u-s4t2ud-a126bdf95a4737cc91f85f57010905df1a2007f6aa0ae83d3dc9893be2851b9d',
+  clientId: import.meta.env.VITE_42_CLIENT_ID,
   redirectUri: 'http://13namima.me/',
   authorizeUrl: 'https://api.intra.42.fr/oauth/authorize',
   tokenUrl: 'https://api.intra.42.fr/oauth/token',
