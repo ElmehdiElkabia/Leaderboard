9b2af0ee04cc9cd3d402942085746732e55198eb
elmehdi elkabia <elmehdielkabia@gmail.com>
2026-07-11 15:31:35 +0100

    feat: Add 2026 year option to the LeaderboardFilters component

--- src/components/filters-simple.jsx
+++ src/components/filters-simple.jsx
@@ -55,6 +55,7 @@ export default function LeaderboardFilters(props) {
     {id: 2023, name: "2023", value: "2023-01-01,2024-01-01"},
     {id: 2024, name: "2024", value: "2024-01-01,2025-01-01"},
     {id: 2025, name: "2025", value: "2025-01-01,2026-01-01"},
+    {id: 2026, name: "2026", value: "2026-01-01,2027-01-01"},
   ];
 
   const handleSearchToggle = () => {
